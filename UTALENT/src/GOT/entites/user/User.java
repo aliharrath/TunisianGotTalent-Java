b831e97debed0e068bd51e033add7972f887ccb1
maha1329 <fathallahmaha29@outlook.fr>
2020-03-27 01:58:03 +0100

    Gestion User
    Registration with mail & login to back and front

---
+++ UTALENT/src/GOT/entites/user/User.java
@@ -0,0 +1,243 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOT.entites.user;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+/**
+ *
+ * @author Freddy
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public User(Integer id, String username, String usernameCanonical, String email, String emailCanonical, boolean enabled, String salt, String password, Timestamp lastLogin, String confirmationToken, Timestamp passwordRequestedAt, String roles, String facebook_id, String typec, int nb_diamants) {
+        this.id = id;
+        this.username = username;
+        this.usernameCanonical = usernameCanonical;
+        this.email = email;
+        this.emailCanonical = emailCanonical;
+        this.enabled = enabled;
+        this.salt = salt;
+        this.password = password;
+        this.lastLogin = lastLogin;
+        this.confirmationToken = confirmationToken;
+        this.passwordRequestedAt = passwordRequestedAt;
+        this.roles = roles;
+        this.facebook_id = facebook_id;
+        this.typec = typec;
+        this.nb_diamants = nb_diamants;
+    }
+
+
+    private Integer id;
+  
+  
+    private String username;
+ 
+    private String usernameCanonical;
+
+    private String email;
+
+    private String emailCanonical;
+
+    private boolean enabled;
+
+    private String salt;
+
+    private String password;
+ 
+    private Timestamp lastLogin;
+  
+    private String confirmationToken;
+ 
+    private Timestamp passwordRequestedAt;
+  
+    private String roles;
+ 
+    private String facebook_id;
+   
+    private String typec;
+    
+    private int nb_diamants;
+
+   
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsernameCanonical() {
+        return usernameCanonical;
+    }
+
+    public void setUsernameCanonical(String usernameCanonical) {
+        this.usernameCanonical = usernameCanonical;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmailCanonical() {
+        return emailCanonical;
+    }
+
+    public void setEmailCanonical(String emailCanonical) {
+        this.emailCanonical = emailCanonical;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Timestamp lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public String getConfirmationToken() {
+        return confirmationToken;
+    }
+
+    public void setConfirmationToken(String confirmationToken) {
+        this.confirmationToken = confirmationToken;
+    }
+
+    public Timestamp getPasswordRequestedAt() {
+        return passwordRequestedAt;
+    }
+
+    public void setPasswordRequestedAt(Timestamp passwordRequestedAt) {
+        this.passwordRequestedAt = passwordRequestedAt;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getFacebook_id() {
+        return facebook_id;
+    }
+
+    public void setFacebook_id(String facebook_id) {
+        this.facebook_id = facebook_id;
+    }
+
+    public String getTypec() {
+        return typec;
+    }
+
+    public void setTypec(String typec) {
+        this.typec = typec;
+    }
+
+    public int getNb_diamants() {
+        return nb_diamants;
+    }
+
+    public void setNb_diamants(int nb_diamants) {
+        this.nb_diamants = nb_diamants;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + '}';
+    }
+
+    public User() {
+    }
+
+    public User(String username, String usernameCanonical, String email, String emailCanonical, boolean enabled, String salt, String password, Timestamp lastLogin, String confirmationToken, Timestamp passwordRequestedAt, String roles, String facebook_id, String typec, int nb_diamants) {
+        this.username = username;
+        this.usernameCanonical = usernameCanonical;
+        this.email = email;
+        this.emailCanonical = emailCanonical;
+        this.enabled = enabled;
+        this.salt = salt;
+        this.password = password;
+        this.lastLogin = lastLogin;
+        this.confirmationToken = confirmationToken;
+        this.passwordRequestedAt = passwordRequestedAt;
+        this.roles = roles;
+        this.facebook_id = facebook_id;
+        this.typec = typec;
+        this.nb_diamants = nb_diamants;
+    }
+    
+    
+    
+}
